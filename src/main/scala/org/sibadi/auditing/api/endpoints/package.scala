dc51888d0146b202993027214b1535c6d0e75d08
KUBR1N <kubrin.m96@gmail.com>
2023-02-06 19:45:32 +0600

    added headers;

--- src/main/scala/org/sibadi/auditing/api/endpoints/package.scala
+++ src/main/scala/org/sibadi/auditing/api/endpoints/package.scala
@@ -9,4 +9,8 @@ package object endpoints {
     .out(header("Access-Control-Allow-Headers", "X-Requested-With, content-type"))
     .out(header("Access-Control-Allow-Credentials", "true"))
     .out(header("Access-Control-Allow-Origin", "*"))
+    .errorOut(header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE"))
+    .errorOut(header("Access-Control-Allow-Headers", "X-Requested-With, content-type"))
+    .errorOut(header("Access-Control-Allow-Credentials", "true"))
+    .errorOut(header("Access-Control-Allow-Origin", "*"))
 }
